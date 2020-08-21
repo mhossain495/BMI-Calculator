46e1d6ab45c9ed589a4593911ebd9d1cecded3d3
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-08-21 18:34:06 -0400

    Update UI Sliders and Labels

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var height = Float(0)
+    var weight = Int(0)
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        height = Float(String(format: "%.2f", sender.value))!
+        heightLabel.text = String(height) + "m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weight = Int(String(format: "%.0f", sender.value))!
+        weightLabel.text = String(weight) + "Kg"
+    }
+    
+    
+    
+    
+    
 }
 
